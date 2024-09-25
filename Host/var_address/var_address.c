55550573d18804dcb1dbb01610952402554cbf6a
AmirMnsur <127439200+AmirMnsur@users.noreply.github.com>
2024-09-25 18:49:38 +0330

    Add files via upload

---
+++ Host/var_address/var_address.c
@@ -0,0 +1,43 @@
+/*
+ *  Created on: September 21, 2024
+ * 		  Author: AmirMnsur
+ *
+ *	>>[ var_address ]<<
+ */
+
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+   char a1 = 'A';
+   /*
+   char a2 = 'p';
+   char a3 = 'p';
+   char a4 = 'l';
+   char a5 = 'e';
+   char a6 = ':'; */
+   //char a7 = ')';
+
+
+
+   unsigned long long int addressOfa1 =  ( unsigned long long int  )&a1;
+
+   printf("Address of variable a1 = %I64x\n",addressOfa1);
+
+
+
+
+    return 0;
+}
+
+
+
